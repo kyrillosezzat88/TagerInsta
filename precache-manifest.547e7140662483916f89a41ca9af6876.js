8f7685fd7b0cbda0efa41914580dbd49c36e2fb3
kyrillos <kyrillos417@gmail.com>
2019-10-16 18:30:14 -0700

    Updates

---
+++ precache-manifest.547e7140662483916f89a41ca9af6876.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc44bd5e953b5fc41a276175cf1c0336",
+    "url": "/TagerInsta/index.html"
+  },
+  {
+    "revision": "5bc48ba5b39d85b56897",
+    "url": "/TagerInsta/static/css/2.c5304fc7.chunk.css"
+  },
+  {
+    "revision": "9a21949e286a87888845",
+    "url": "/TagerInsta/static/css/main.8e4190f1.chunk.css"
+  },
+  {
+    "revision": "5bc48ba5b39d85b56897",
+    "url": "/TagerInsta/static/js/2.39d3d6d7.chunk.js"
+  },
+  {
+    "revision": "9a21949e286a87888845",
+    "url": "/TagerInsta/static/js/main.2e4624df.chunk.js"
+  },
+  {
+    "revision": "1de3f63a928352b2b9e3",
+    "url": "/TagerInsta/static/js/runtime-main.5e352f32.js"
+  },
+  {
+    "revision": "51e11dce78c5d122306d90326981b980",
+    "url": "/TagerInsta/static/media/ar.51e11dce.png"
+  },
+  {
+    "revision": "a5e4bb54fa761da6e4cf90d7441e3570",
+    "url": "/TagerInsta/static/media/logo.a5e4bb54.png"
+  },
+  {
+    "revision": "3fdd7970b02604b93df9d5d8535f5ad7",
+    "url": "/TagerInsta/static/media/myFatorah.3fdd7970.png"
+  },
+  {
+    "revision": "3ab3d03578e85e42e2fa6fdbdcd1da2c",
+    "url": "/TagerInsta/static/media/paypal.3ab3d035.png"
+  }
+]);
